fb50fcf6b32690bddbd7baef8a9f526609fc239d
PinkishFrog4542 <PinkishFrog4542@github.com>
2020-03-24 16:34:37 -0500

    AddTwoNumbers from Sibi Santharaam

---
+++ src/main/java/com/sibi/challenge2/AddTwoNumbers.java
@@ -0,0 +1,24 @@
+package com.sibi.challenge2;
+
+public class AddTwoNumbers {
+
+    public static void main(String[] args) {
+
+        int a = 2;
+        int b = 3;
+
+        double c = 50.5;
+        double d = 38.9;
+
+
+        String x = "Sibi";
+        String y = " is the best";
+
+        System.out.println(x+y);
+        System.out.println(c+d);
+        System.out.println(a+b);
+
+
+    }
+
+}
