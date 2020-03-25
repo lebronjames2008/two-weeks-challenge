79f65929bf40393288d25f2f4109aa80aaa13534
PinkishFrog4542 <PinkishFrog4542@github.com>
2020-03-25 18:33:02 -0500

    MultiplyTwoNumbers from Sibi Santharaam

---
+++ src/main/java/com/sibi/challenge3/MultiplyTwoNumbers.java
@@ -0,0 +1,15 @@
+package com.sibi.challenge3;
+
+public class MultiplyTwoNumbers {
+
+    public static void main(String[] args) {
+
+        double number1 = 3.908;
+        double number2 = 500908.9876547638;
+
+        double result = number1 * number2;
+
+        System.out.println(result);
+    }
+}
+
