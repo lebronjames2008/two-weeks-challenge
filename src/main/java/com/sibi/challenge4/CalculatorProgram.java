75313e53942c186b5a6963d5a656ee8aa1016919
PinkishFrog4542 <PinkishFrog4542@github.com>
2020-03-26 16:12:22 -0500

    CalculatorProgram from Sibi Santharaam

---
+++ src/main/java/com/sibi/challenge4/CalculatorProgram.java
@@ -0,0 +1,29 @@
+package com.sibi.challenge4;
+
+public class CalculatorProgram {
+
+    public static void main(String[] args) {
+
+
+        int number1 = 52;
+        int number2 = 37;
+
+        int result = number1+number2;
+        System.out.println("Result for addition is "+ result);//"Result for addition is
+
+        //-------------------------
+
+        int answer = number1-number2;
+        System.out.println("Result for subtraction is "+ answer);
+
+        //--------------------------
+
+        int solution = number1/number2;
+        System.out.println("Result for division is "+solution);
+        //--------------------------
+
+        int formation = number1*number2;
+        System.out.println("Result for multiplication is "+formation);
+
+    }
+}
