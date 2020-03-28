f118b03fc2c236f8536c248bfa95fb0b986e0e9a
PinkishFrog4542 <PinkishFrog4542@github.com>
2020-03-27 20:30:00 -0500

    BooleanVariable from Sibi Santharaam

---
+++ src/main/java/com/sibi/challenge5/BooleanVariable.java
@@ -0,0 +1,21 @@
+package com.sibi.challenge5;
+
+import javax.print.attribute.standard.PresentationDirection;
+
+public class BooleanVariable {
+    public static void main(String[] args) {
+
+        boolean colors;
+        colors = true;
+
+        if (colors = true) {
+            System.out.println("green");
+        }
+        else {
+            System.out.println("red");
+
+        }
+
+
+    }
+}
