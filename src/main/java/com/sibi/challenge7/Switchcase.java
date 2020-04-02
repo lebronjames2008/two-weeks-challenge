b9e4b3b2199aac7ea1c37311433d3d1e7d898cf1
PinkishFrog4542 <PinkishFrog4542@github.com>
2020-04-02 12:56:19 -0500

    Switchcase from Sibi Santharaam

---
+++ src/main/java/com/sibi/challenge7/Switchcase.java
@@ -0,0 +1,28 @@
+package com.sibi.challenge7;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Switchcase {
+    public static void main(String args[]) {
+        int month = 2;
+        switch (month) {
+
+            case 1:
+                System.out.println("January");
+                break;
+
+            case 2:
+                System.out.println("April");
+                break;
+            case 3:
+                System.out.println("March");
+                break;
+
+
+
+        }
+    }
+}
+
+
+
