6983d7bd2875056eef1d069ef21da357f9f98c63
PinkishFrog4542 <PinkishFrog4542@github.com>
2020-04-03 13:38:48 -0500

    Name 10 times from Sibi Santharaam

---
+++ src/main/java/com/sibi/challenge8/Name10real.java
@@ -0,0 +1,11 @@
+package com.sibi.challenge8;
+
+public class Name10real {
+    public static void main(String[] args) {
+        for (int myname = 0; myname < 10; myname++) {
+            System.out.println("Sibi");
+        }
+
+    }
+
+}
--- src/main/java/com/sibi/challenge8/test.java
+++ src/main/java/com/sibi/challenge8/test.java
@@ -1,12 +1,9 @@
 package com.sibi.challenge8;
 
-public class Name10 {
+public class test {
     public static void main(String[] args) {
-        int sibi;
-        for (sibi = 2; sibi < 5; sibi++){
+        for(int sibi=4; sibi>1; sibi--){
             System.out.println(sibi);
-
-
         }
     }
 }
