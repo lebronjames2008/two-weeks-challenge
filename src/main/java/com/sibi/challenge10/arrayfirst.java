833332ae595465041f4f285a975bb27850afc8a6
PinkishFrog4542 <PinkishFrog4542@github.com>
2020-04-04 15:27:53 -0500

    String Array from Sibi Santharaam

---
+++ src/main/java/com/sibi/challenge10/arrayfirst.java
@@ -0,0 +1,12 @@
+package com.sibi.challenge10;
+
+public class arrayfirst {
+    public static void main(String[] args) {
+
+        String[] friends = { "Sachin", "Henry", "Vivek", "Surya","Kathir","More Friends" };
+
+
+        for (String str : friends) {
+            System.out.println(str);
+        }
+    }}
