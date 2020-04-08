ec91b45df4c6467336cd115befc601b6097c3619
PinkishFrog4542 <PinkishFrog4542@github.com>
2020-04-08 13:12:59 -0500

    CalculatorObject from Sibi Santharaam

---
+++ src/main/java/com/sibi/challenge15/Calculator1.java
@@ -0,0 +1,31 @@
+package com.sibi.challenge15;
+
+public class Calculator1 {
+    public static void yes() {
+        int number1 = 52;
+        int number2 = 37;
+        int answer = number1-number2;
+        System.out.println("Result for subtraction is "+ answer);
+
+    }
+    public static void Calculator2(){
+        int number1 = 52;
+        int number2 = 37;
+        int result = number1+number2;
+        System.out.println("Result for addition is "+ result);
+    }
+    public static void Calculator3(){
+        int number1 = 52;
+        int number2 = 37;
+        int solution = number1/number2;
+        System.out.println("Result for division is "+solution);
+
+    }
+    public static void Calculator4(){
+        int number1 = 52;
+        int number2 = 37;
+        int formation = number1*number2;
+        System.out.println("Result for multiplication is "+formation);
+
+    }
+    }
---
+++ src/main/java/com/sibi/challenge15/MyExample4.java
@@ -0,0 +1,21 @@
+package com.sibi.challenge15;
+
+public class MyExample4 {
+    public static void main(String[] args) {
+
+        Calculator1 Calculators = new Calculator1();
+
+        Calculators.yes();
+
+        Calculators.Calculator2();
+
+        Calculators.Calculator3();
+
+        Calculators.Calculator4();
+
+
+
+
+
+    }
+}
